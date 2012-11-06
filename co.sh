0681f637abf73a8f66fd367ef8c70add59489766
Amiruddin Nagri <amir.nagri+git@gmail.com>
2012-11-06 12:24:57 +0530

    amir/sarah - script to checkout remote branches as local

---
+++ co.sh
@@ -0,0 +1,8 @@
+git checkout -b 1 origin/1_align_user_info
+git checkout -b 2 origin/2_align_header
+git checkout -b 3 origin/3_align_nav
+git checkout -b 4 origin/4_align_sub_nav
+git checkout -b 5 origin/5_align_content
+git checkout -b 6 origin/6_align_footer
+git checkout -b 7 origin/7_box_model
+git checkout -b 8 origin/8_fix_header
